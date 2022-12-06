24f50dab38b139ecd789b1841a1a5d33d662ecbf
Armin <armin3600z@gmail.com>
2022-12-06 18:56:29 +0000

    Added grid and tile classes

---
+++ Grid.cs
@@ -0,0 +1,11 @@
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Birdle
+{
+    internal class Grid
+    {
+        
+    }
+}
