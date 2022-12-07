df1c726817ade6b4bf21f847984aed08f5f4e012
Armin Raad <45851390+Deltius5kb@users.noreply.github.com>
2022-12-07 19:59:28 +0000

    Cant quit using backspace anymore

--- Game1.cs
+++ Game1.cs
@@ -44,9 +44,6 @@ namespace Birdle
 
         protected override void Update(GameTime gameTime)
         {
-            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
-                Exit();
-
             // TODO: Add your update logic here
 
             base.Update(gameTime);
